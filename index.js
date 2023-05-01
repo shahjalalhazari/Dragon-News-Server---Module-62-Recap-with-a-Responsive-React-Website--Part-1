cd8f477968adf5ad7114c92a533d4998b35b1140
Shahjalal Hazari <102680927+shahjalalhazari@users.noreply.github.com>
2023-05-01 18:45:22 +0400

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,27 @@
 const express = require('express');
 const app = express();
 const port = 3000;
+const cors = require('cors');
+
+app.use(cors());
+
+const categories = require('./data/categories.json');
+const news = require('./data/news.json');
+
 
 app.get('/', (req, res) => {
-    res.send("Helloo Wordl");
+    res.send(`Dragon Server Is Running on ${port}`);
 });
+
+
+app.get('/categories', (req, res) => {
+    res.send(categories);
+});
+
+
+app.get('/news', (req, res) => {
+    res.send(news);
+})
 
 
 app.listen(port, () => {
